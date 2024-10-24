1c62e445bf4a05cdd937846f84edbfbd8d795422
Paula <pafernandez10@gmail.com>
2024-10-24 12:02:24 +0200

    new comments

--- Carrito.js
+++ Carrito.js
@@ -3,14 +3,25 @@ export default class Carrito {
   constructor(productos) {
     this.productos = [];
   }
+      //Primero, este método nos permitirá ir actualizando nuestro carrito
+      //cada vez que el valor del input cambie. Para ello, le pasamos primero
+      //las variables que necesitaremos que muestre a la hora de actualizarlo:
+      //su SKU, la cantidad, el nombre y el precio. A continuación, establecemos
+      //una nueva variable que primero se recorra el array de productos y busque 
+      //el producto cuyo SKU coincida con el SKU que reciba.
       actualizarUnidades(SKU, unidades, title, price) {
         let productoEncontrado = this.productos.find(function (producto){
           return producto.SKU === SKU;
           
         });
+        //En caso de encontrarse, le diremos que únicamente modifique la cantidad.
+        //Es decir, en caso de que ese producto ya se haya añadido al carrito y que,
+        //por consiguiente, el SKU ya aparezca dentro, solo aumentaremos o reduciremos
+        //la cantidad.
           if (productoEncontrado) {
             productoEncontrado.cantidad += unidades;
           }
+          //Si ese producto aún no se ha añadido al carrito lo incluiremos con sus datos. 
           else {
             this.productos.push({
               SKU, 
@@ -20,32 +31,46 @@ export default class Carrito {
               });
           }
         }
-    
+        
+        //Esta función finalmente no se ha utilizado en el script porque dentro de 'actualizarUnidades'
+        //se ha llamado también a la información de dicho producto. 
         obtenerInformacionProducto(sku) {
 
-          let productoEncontrado = this.productos.find(function(producto){
-            producto.SKU === sku;
-          });
+          // let productoEncontrado = this.productos.find(function(producto){
+          //   producto.SKU === sku;
+          // });
 
-            if (productoEncontrado) {
-              return {
-                SKU: productoEncontrado.SKU,
-                cantidad: productoEncontrado.cantidad
-              };
-            }
-            else {
-              return "Producto no encontrado";
-            }
+          //   if (productoEncontrado) {
+          //     return {
+          //       SKU: productoEncontrado.SKU,
+          //       cantidad: productoEncontrado.cantidad
+          //     };
+          //   }
+          //   else {
+          //     return "Producto no encontrado";
+          //   }
         }
-    
+        
+        //Esta función es algo más genérica, nos permite, además de calcular el total
+        //del carrito, devolver un objeto carrito con el total y los productos. 
+        //Me supuso algunos problemas porque no era necesario solo con esta función
+        //para poder mostrar el total. Es necesaria para que se muestren los productos
+        //en la web y, para poder mostrar el resultado de la operación del total recurrí
+        //a la creación otra función más específica. 
+
         obtenerCarrito() {
+          //Definimos la variable finalFinal para mostrar el calculo total dentro de ella. 
+          //Usamos un .reduce para agrupar los elementos del array producto en un único valor.
         let finalFinal = this.productos.reduce(function(acumulador, producto) {
           return acumulador + (producto.cantidad * producto.price);
         }, 0);
         
+        //Devolvemos primero el objeto completo con el total obtenido y los productos
         return {
           total: finalFinal.toFixed(2),
           currency: "€",
+          //Recorremos los productos con .map que nos devolverá un nuevo objeto para 
+          //cada producto con las variables que le especificamos. Es la lista de productos transformada.
           productos: this.productos.map(function(producto) {
             return {
             sku: producto.SKU,
@@ -58,6 +83,8 @@ export default class Carrito {
         }
       }
 
+      //Esta función fue necesaria para poder mostrar en la web 
+      //el calculo total de los productos.
       sacarTotalCarrito (){
         let carritoTotal = this.productos.reduce(function(acumulador, producto) {
           return acumulador + (producto.cantidad * producto.price);
--- script.js
+++ script.js
@@ -2,19 +2,22 @@ import Carrito from "./Carrito.js";
 const carrito = new Carrito();
 var productos = [];
 document.addEventListener('DOMContentLoaded', function(event) {
-
-   
+    //Función que carga la tabla donde aparecerán los productos y las respectivas
+    //cantidades a través de buttons que el usuario elija, así como el precio total
+    //del producto multiplicado por esa cantidad.
     function cargarTabla(productos) {
 
         const tablaProductos = document.getElementById("cuerpoTabla");
 
+        //De esta forma recorreremos los productos.
         productos.forEach(producto => {
 
             //columna que junta dos datos del API: título y referencia.
             const titleSku = document.createElement('td');
-            titleSku.innerHTML = `${producto.title}<br><p>REF:</p>${producto.SKU}`;
+            titleSku.innerHTML = `<br>${producto.title}<br>REF:${producto.SKU}<br><br>`;
             
-            //fila cantida, botón (+), (-) e input.
+            //fila cantidad, botón (+) y (-) para seleccionar la cantidad
+            //de producto e input.
             const cantidad = document.createElement('td');
             const botonCantidadMas = document.createElement('button');
             botonCantidadMas.innerText = '+';
@@ -27,19 +30,21 @@ document.addEventListener('DOMContentLoaded', function(event) {
             cantidad.append(botonCantidadMas, inputCantidad, botonCantidadMenos);
 
         
-            //función que aumente la cantidad al pulsar botón más:
-            //el +inputCantidad cambia a number la variable para poder indicarle, en este caso,
-            //la condición de que no puede estar en negativo.
+            //evento que aumenta la cantidad al hacer click en el botón más:
             botonCantidadMas.addEventListener('click', function(event) {
                 inputCantidad.value ++;
-                //Le pasamos los valores que recibía el método en la clase: SKU y el valor incrementado
-                //que hemos guardado en inputCantidad.
+                //Le pasamos los valores que recibe el método en nuestra clase Carrito: SKU 
+                // title y precio, así como el valor incrementado que hemos guardado en inputCantidad.
                 carrito.actualizarUnidades(producto.SKU, 1, producto.title, producto.price);
+                //Es muy importante que llamemos a los métodos para que estos datos se almacenen.
                 calculoTotal();
                 cargarCarrito();
             });
 
-
+            //Segundo evento para el botón menos donde le indicamos, especificando
+            //con '+' que inputCantidad tiene un valor numérico, que siempre que
+            //la cantidad sea mayor que 0 nos decremente la cantidad para nunca 
+            //trabajar con métodos negativos.
             botonCantidadMenos.addEventListener('click', function(event) {
                 if(+inputCantidad.value > 0){
                     inputCantidad.value = +inputCantidad.value -1; 
@@ -50,6 +55,7 @@ document.addEventListener('DOMContentLoaded', function(event) {
                 
             });
 
+            //columna para el precio a la que le introducimos el valor 'price' de la API.       
             const price = document.createElement('td');
             price.innerText = producto.price;
 
@@ -58,12 +64,18 @@ document.addEventListener('DOMContentLoaded', function(event) {
             const total = document.createElement('td');
             total.innerText = '0';
 
+            //A esta función recurriremos en ambos eventos de los botones (+) y (-).
+            //Creamos dos variables definiendo en primer lugar la cantidadTotal como
+            //el valor numérico de la cantidad que aparezca en nuestro input. Y, en 
+            //segundo lugar, el precio del producto para poder introducir el calculo 
+            //en nuestra columna del total.
             function calculoTotal () {
                 let cantidadTotal = +inputCantidad.value;
                 let precioProducto = producto.price;
                 total.innerText = (cantidadTotal * precioProducto).toFixed(2);
             }
 
+            //Por último, introducimos todos nuestros datos dentro de un 'tr. 
             const tr = document.createElement('tr');
             tr.append(titleSku, cantidad, price, total);
 
@@ -71,37 +83,41 @@ document.addEventListener('DOMContentLoaded', function(event) {
         })
     }
 
-    
+    //Esta función primero nos mostrará el apartado del total de productos elegidos,
+    //además del precio unitario de cada uno y su nombre. Después, para terminar, 
+    //mostrará el total final del carrito, es decir, la suma del total obtenido por producto.
     function cargarCarrito(productos) {
 
-
+        //Inicializamos vacío nuestro elemento 'div' del html, para que al pulsar
+        //sobre los productos estos se acumulen, no que se muestren por separado 
+        //cada vez que pulsemos. 
         let cesta = document.getElementById("cesta");
         cesta.innerHTML = "";
 
-        //Recoge los datos del objeto carrito.
+        //Recogemos los datos del objeto carrito con nuestro método ubicado en la clase.
         let infoCarrito = carrito.obtenerCarrito();
+        //Recorremos los productos para que, al pulsar sobre los botones nos muestre directamente
+        //los valores 'title', 'cantidad' y 'price'. Sin olvidarnos de introducir con un append
+        //toda esta información dentro de nuestro div 'cesta' especificado previamente. 
         infoCarrito.productos.forEach(function(producto){
             let prod = document.createElement('p');
             prod.innerText = `${producto.title} x${producto.cantidad} Total: ${producto.price}€`;
             cesta.append(prod);
         });
 
-
+        //De esta forma añadimos nuestro total final, después de haber creado
+        //prreviamente una nueva función en nuestra clase Carrito que nos devuelva
+        //la operación necesaria. Será necesario, de nuevo, incluir con un append
+        //esta nueva sección. 
         let totalFinal = document.createElement('div');
         totalFinal.innerText = `Total: ${carrito.sacarTotalCarrito()}€`;
 
         cesta.append(totalFinal);
-
-        document.getElementById("cantidadTotal").innerText = `Precio Total: ${carrito.sacarTotalCarrito()}€`;
-        // let totalFinal = document.createElement('p');
-        // totalFinal.innerHTML = `Total: ${infoCarrito.total}€`;
-        // cesta.append(totalFinal);
         
-        // document.getElementById("cantidadTotal").innerText = `Precio Total: ${infoCarrito.total}${infoCarrito.currency}`
         };
    
 
-
+    
     fetch('https://jsonblob.com/api/1296529608542117888')
     .then(response => response.json())
         .then(products => {
